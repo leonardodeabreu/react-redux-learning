da7cd8cbbe4b0adcc3e7c04f0f5373c5699bfb68
Leonardo Abreu <leonardo.abreu@db1.com.br>
2018-04-20 14:20:34 -0300

    #reat2

---
+++ exercicios_react/ex.6/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ClassComponent from './classComponent'
+
+//app renderizado no index.html
+ReactDOM.render(
+    
+    <ClassComponent value="teste"/>
+
+, document.getElementById('app'))
